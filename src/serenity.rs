ded5532d0301766e545072437eda97aba8d57ed9
personinblack <berkay@tuta.io>
2021-05-16 01:12:34 +0300

    Remove document caching

--- src/serenity.rs
+++ src/serenity.rs
@@ -1,23 +1,16 @@
-use std::{collections::HashMap, sync::Arc};
+use std::sync::Arc;
 
-use chrono::Duration;
 use futures::lock::Mutex;
 use serenity::{
     client::{ClientBuilder, Context},
-    model::{
-        guild::Member,
-        id::{GuildId, UserId},
-    },
+    model::guild::Member,
     prelude::TypeMapKey,
 };
-use tokio::sync::RwLock;
 
 use crate::Document;
 
-type Documents = RwLock<HashMap<(UserId, GuildId), Arc<Mutex<Document>>>>;
 pub struct NicknameDb {
     prefix: char,
-    documents: Documents,
 }
 
 impl TypeMapKey for NicknameDb {
@@ -26,50 +19,10 @@ impl TypeMapKey for NicknameDb {
 
 impl NicknameDb {
     pub async fn get_document(&self, member: Member) -> Arc<Mutex<Document>> {
-        let mut documents = self.documents.write().await;
-        let ug = (member.user.id, member.guild_id);
-        if let Some(document) = documents.get(&ug) {
-            let documentl = document.lock().await;
-            if documentl.name == member.display_name().to_string() {
-                return document.clone();
-            }
-        }
-
-        let document = Arc::new(Mutex::new(Document::new(
+        Arc::new(Mutex::new(Document::new(
             member.display_name().to_string(),
             self.prefix,
-        )));
-        documents.insert(ug, document);
-        let document = documents.get(&ug).unwrap().clone();
-        drop(documents);
-        self.clear_cache().await;
-        document
-    }
-
-    pub async fn remove_document(&self, member: Member) {
-        let mut documents = self.documents.write().await;
-        let ug = (member.user.id, member.guild_id);
-        documents.remove(&ug);
-    }
-
-    async fn clear_cache(&self) {
-        let mut to_remove: Vec<(UserId, GuildId)> = vec![];
-
-        {
-            let documents = self.documents.read().await;
-            for (member, document) in documents.iter().step_by(2) {
-                if let Some(document) = document.try_lock() {
-                    if document.since_last_access().await > Duration::minutes(1) {
-                        to_remove.push(*member);
-                    }
-                }
-            }
-        }
-
-        let mut documents = self.documents.write().await;
-        for member in to_remove {
-            documents.remove(&member);
-        }
+        )))
     }
 }
 
@@ -79,10 +32,7 @@ pub trait SerenityInit {
 
 impl SerenityInit for ClientBuilder<'_> {
     fn register_nicknamedb(self, prefix: char) -> Self {
-        self.type_map_insert::<NicknameDb>(Arc::new(NicknameDb {
-            prefix,
-            documents: Default::default(),
-        }))
+        self.type_map_insert::<NicknameDb>(Arc::new(NicknameDb { prefix }))
     }
 }
 
